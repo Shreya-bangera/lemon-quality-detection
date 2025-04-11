dcca409018d66b9bf3275601ede525db68807b88
shreya bangera <shreyas85226@gmail.com>
2025-04-11 12:29:37 +0530

    initial commit

---
+++ home/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('registration/', views.registration, name='registration'),
+    path('prediction/', views.prediction, name='prediction'),
+    path('faqs/',views.faqs,name='faqs'),
+    
+]
